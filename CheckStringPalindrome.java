22bd5ebc88757687cfcf7c6f5530ba6875b68722
Akash Upadhyay <aagamerstudy@gmail.com>
2026-06-21 21:34:53 +0530

    3rd practice question

---
+++ CheckStringPalindrome.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class CheckStringPalindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        int i = 0;
+        int j = s.length()-1;
+
+        while(i<=j){
+            if(s.charAt(i) != s.charAt(j)){
+                System.out.println(false);
+                return;
+            }
+            i++;
+            j--;
+        }
+        System.err.println(true);
+        
+    }
+    
+}
